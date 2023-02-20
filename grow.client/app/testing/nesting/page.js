c7d82fa08d39969b740be4ffbc4123f3b488d04f
Matt Daisley <mattdaisley@gmail.com>
2023-02-20 14:51:27 -0700

    full page-view-field traversal from subscriptions

--- grow.client/app/testing/nesting/page.js
+++ grow.client/app/testing/nesting/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { Children, cloneElement, Fragment, isValidElement, useRef, useState, useEffect, useContext, useMemo, useCallback } from "react";
-import { FormProvider, useForm, Controller, useFormContext, useFieldArray } from "react-hook-form";
+import { FormProvider, useForm, Controller, useFormContext, useFieldArray, useWatch } from "react-hook-form";
 import { v4 as uuidv4 } from 'uuid';
 
 import AddIcon from '@mui/icons-material/Add';
@@ -28,6 +28,7 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import logger from "../../../../grow.api/src/logger";
 import { SocketContext } from "../../SocketContext";
+import { unflatten } from 'flat';
 
 function useItems(itemKeys) {
   logger.log('useItems', itemKeys)
@@ -37,8 +38,9 @@ function useItems(itemKeys) {
   const itemsRef = useRef({ itemKeys })
   itemsRef.current = {
     itemKeys,
+    data: {},
     broadcasted: {},
-    data: { 'test.label': "loading" },
+    subscriptions: {}
   }
 
   const formMethods = useForm();
@@ -47,6 +49,15 @@ function useItems(itemKeys) {
   itemsRef.current.getData = (name) => {
     logger.log('getData', name, itemsRef.current.data?.[name])
     return itemsRef.current.data?.[name];
+  }
+
+  itemsRef.current.getNestedData = (name) => {
+    // logger.log('getNestedData', name, itemsRef.current.data)
+    const nestedData = {};
+    Object.keys(itemsRef.current.data)
+      .filter((dataKey) => dataKey.startsWith(name))
+      .map(dataKey => nestedData[dataKey] = itemsRef.current.data[dataKey])
+    return nestedData
   }
 
   itemsRef.current.broadcast = (valueKey, event, value) => {
@@ -65,12 +76,63 @@ function useItems(itemKeys) {
         break;
     }
 
+    runSubscriptions(itemsRef.current.subscriptions, itemsRef.current.data, valueKey, newValue)
+
     itemsRef.current.data[valueKey] = newValue
     itemsRef.current.broadcasted[valueKey] = newValue
 
     const setItemsEvent = { itemKey, values: { [valueKey]: newValue } }
     socket?.emit('set-items', setItemsEvent)
     logger.log('broadcast socket emit set-item:', itemKey, setItemsEvent)
+  }
+
+  itemsRef.current.subscribe = (valueKey, callback) => {
+    itemsRef.current.subscriptions[valueKey] = callback;
+  }
+
+  function runSubscriptions(subscriptions, data, valueKey, value) {
+    logger.log('runSubscriptions', subscriptions, valueKey, value, data)
+    // if (subscriptions.hasOwnProperty(valueKey)) {
+    // const callback = subscriptions[valueKey]
+    // callback(valueKey, value)
+    // }
+    // else {
+    Object.keys(subscriptions).map(subscriptionKey => {
+      if (subscriptionKey === valueKey) {
+        const callback = subscriptions[valueKey]
+        callback(valueKey, value)
+      }
+      else if (valueKey.startsWith(subscriptionKey)) {
+        const nestedDataKeys = {};
+        Object.keys(data)
+          .filter((dataKey) => dataKey.startsWith(subscriptionKey))
+          .map(dataKey => nestedDataKeys[dataKey] = data[dataKey])
+        logger.log('runSubscriptions nestedData', subscriptionKey, valueKey, nestedDataKeys)
+        if (nestedDataKeys.length === 0 || !nestedDataKeys.hasOwnProperty(valueKey)) {
+          // an item was updated
+          const callback = subscriptions[subscriptionKey]
+          callback(valueKey, value)
+        }
+        else {
+          // a new item was added
+        }
+      }
+    })
+
+    // Object.keys(data).map(dataKey => {
+    //   if (valueKey.startsWith(dataKey)) {
+    //     const callback = subscriptions[subscriptionKey]
+    //     callback(valueKey, value)
+    //   }
+    // })
+
+    // Object.keys(subscriptions).map(subscriptionKey => {
+    //   if (valueKey.startsWith(subscriptionKey)) {
+    //     const callback = subscriptions[subscriptionKey]
+    //     callback(valueKey, value)
+    //   }
+    // })
+    // }
   }
 
   useEffect(() => {
@@ -107,8 +169,10 @@ function useItems(itemKeys) {
         const value = item.value
         const broadcastedValue = itemsRef.current.broadcasted[valueKey]
         if (broadcastedValue === undefined) {
+          newData[valueKey] = value;
           const { setValue } = itemsRef.current.formMethods
           setValue(valueKey, value)
+          runSubscriptions(itemsRef.current.subscriptions, itemsRef.current.data, valueKey, value)
         }
         else if (broadcastedValue === value) {
           delete itemsRef.current.broadcasted[valueKey]
@@ -122,7 +186,7 @@ function useItems(itemKeys) {
     itemsRef.current.data = {
       ...itemsRef.current.data,
       ...newData
-    };
+    }
     logger.log('handleReceiveAllItems newData', itemsRef.current.data)
   }
 
@@ -131,24 +195,182 @@ function useItems(itemKeys) {
 
 export default function TestingNestingPage() {
 
-  const items = useItems(['test0', 'test1']);
+  // const itemKeys = ['pages', 'test0', 'test1']
+  const itemKeys = ['pages', 'views', 'fields']
+
+
+  const items = useItems(itemKeys);
 
   logger.log('TestingNestingPage', items)
+  const contexts = ['pages', 'sections', 'views', 'groups', 'fields']
   const types = ['text', 'autocomplete']
+  const randomWords = ['guest', 'art', 'mud', 'length', 'dirt', 'child', 'lab', 'depth', 'bath']
 
   const names = []
   for (let i = 0; i < 10; i++) {
-    names.push({ name: `test${Math.floor(i / 5)}.${Math.floor(i / 2)}`, type: types[Math.round(i % 2)] })
+    const itemIndex = Math.floor(i / 5)
+    const prefix = contexts[Math.round(i % 3)]
+    const randomWord = randomWords[Math.floor(i / 2)]
+    const type = types[Math.round(i % 2)]
+    names.push({ name: `test${itemIndex}.${prefix}.${randomWord}`, type })
   }
 
   return (
-    <Box sx={{ p: 2 }}>
-      {names.map((name, index) => (
-        <NestLevel1 key={index} {...name} {...items} />
-      ))}
-    </Box>
+    <Grid xs={12} container sx={{ width: '100%' }}>
+      <Grid xs={6}>
+        <ShowItems itemKey={'pages'} {...items} />
+      </Grid>
+      <Grid xs={6} sx={{ p: 2 }}>
+        {/* {names.map((name, index) => (
+          <NestLevel1 key={index} {...name} {...items} />
+        ))} */}
+      </Grid>
+    </Grid>
   )
 }
+
+function ShowItems({ searchSuffix, ...props }) {
+  const [fields, setFields] = useState({})
+  const _fieldsRef = useRef({})
+
+  logger.log('ShowItems', 'keyPrefix:', props.keyPrefix, 'itemKey:', props.itemKey, 'searchSuffix:', searchSuffix, 'fields:', fields, 'props:', props)
+
+  let name = props.itemKey
+  if (props.keyPrefix) {
+    name = `${props.keyPrefix}.${name}`
+  }
+  let searchName = name;
+  if (searchSuffix) {
+    searchName = `${name}.${searchSuffix}`
+  }
+
+  useEffect(() => {
+    const nestedData = props.getNestedData(searchName)
+    const newFields = {}
+    Object.keys(nestedData).map(nestedDataKey => {
+      const trimmedKey = nestedDataKey.substring(name.length + 1, nestedDataKey.length)
+      newFields[trimmedKey] = nestedData[nestedDataKey]
+    })
+    if (_fieldsRef?.current !== undefined) {
+      _fieldsRef.current = newFields
+      setFields(unflatten(_fieldsRef.current, { object: true }))
+    }
+    logger.log('ShowItems useEffect', 'name:', name, 'newFields:', newFields)
+  }, [name])
+
+  props.subscribe(name, (valueKey, value) => {
+    // logger.log('ShowItems subscribed change', name, valueKey, value)
+    const trimmedKey = valueKey.substring(name.length + 1, valueKey.length)
+
+    if (_fieldsRef?.current !== undefined) {
+      _fieldsRef.current = {
+        ..._fieldsRef.current,
+        [trimmedKey]: value
+      }
+      setFields(unflatten(_fieldsRef.current, { object: true }))
+    }
+  })
+
+  if (Object.keys(fields).length === 0) {
+    return null;
+  }
+
+  return (
+    <ShowControls {...props} name={name} fields={fields} />
+  )
+}
+
+function ShowControls({ name, fields, ...props }) {
+  logger.log('ShowControls', 'name:', name, 'itemKey:', props.itemKey, 'fields:', fields)
+  return (
+    <>
+      {Object.keys(fields).map(fieldKey => {
+        const itemKeys = fields[fieldKey]
+        logger.log('ShowControls rendering', 'fieldKey:', fieldKey, 'itemKeys:', itemKeys)
+        switch (props.itemKey) {
+          case 'pages':
+          case 'sections':
+            return (
+              <EditPage key={fieldKey} {...props} keyPrefix={`${name}.${fieldKey}`} fieldKey={fieldKey} itemKeys={itemKeys} />
+            )
+          case 'views':
+          case 'groups':
+          case 'fields':
+            return (
+              <EditView key={fieldKey} {...props} keyPrefix={`${name}.${fieldKey}`} fieldKey={fieldKey} itemKeys={itemKeys} />
+            )
+
+          default:
+            return <div key={fieldKey}>{props.itemKey}: <TextField fullWidth multiline value={JSON.stringify(itemKeys, null, 2)} /></div>
+        }
+      })}
+    </>
+  )
+}
+
+function EditPage({ keyPrefix, fieldKey, itemKeys, ...props }) {
+  logger.log('EditPage', 'itemKey:', props.itemKey, 'keyPrefix:', keyPrefix, 'fieldKey:', fieldKey, 'itemKeys:', itemKeys, 'props:', props)
+  const { name: nameProperty, label: labelProperty, width: widthProperty, ...rest } = itemKeys;
+  return (
+    <div key={fieldKey} style={{ border: '1px solid black', padding: '10px' }}>
+      <div>{props.itemKey}.{fieldKey}</div>
+      <div style={{ padding: '10px' }}>
+        <EditProperty {...props} controllerName={`${keyPrefix}.name`} label="Name" />
+        <EditProperty {...props} controllerName={`${keyPrefix}.label`} label="Label" />
+        <EditProperty {...props} controllerName={`${keyPrefix}.width`} label="Width" />
+      </div>
+      <div style={{ paddingLeft: '10px' }}>
+        {Object.keys(rest).map(itemKey => {
+          return (
+            <ShowItems key={itemKey} {...props} keyPrefix={keyPrefix} itemKey={itemKey} />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+function EditView({ keyPrefix, fieldKey, itemKeys, ...props }) {
+  logger.log('EditView', 'itemKey:', props.itemKey, 'keyPrefix:', keyPrefix, 'fieldKey:', fieldKey, 'itemKeys:', itemKeys, 'props:', props)
+  const { id: idProperty, name: nameProperty, label: labelProperty, width: widthProperty, ...rest } = itemKeys;
+  return (
+    <div key={fieldKey} style={{ border: '1px solid black', padding: '10px' }}>
+      {
+        idProperty !== undefined
+          ? <ShowItems {...props} keyPrefix={undefined} itemKey={props.itemKey} searchSuffix={itemKeys.id} />
+          : (
+            <>
+              <div>{props.itemKey}.{fieldKey}</div>
+              <div style={{ padding: '10px' }}>
+                <EditProperty {...props} controllerName={`${keyPrefix}.name`} label="Name" />
+                <EditProperty {...props} controllerName={`${keyPrefix}.label`} label="Label" />
+                <EditProperty {...props} controllerName={`${keyPrefix}.width`} label="Width" />
+              </div>
+            </>
+          )
+      }
+      <div style={{ paddingLeft: '10px' }}>
+        {Object.keys(rest).map(itemKey => {
+          logger.log('Edit view nested', itemKey)
+          return (
+            <ShowItems key={itemKey} {...props} keyPrefix={keyPrefix} itemKey={itemKey} />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+function EditProperty({ controllerName, label, ...props }) {
+  return <FieldItem
+    {...props}
+    name={controllerName}
+    render={(nextProps) => {
+      return <ControlledTextField {...nextProps} label={label} />
+    }}
+  />
+}
+
 
 function NestLevel1({ children, ...props }) {
   logger.log('NestLevel1', props)
@@ -208,7 +430,7 @@ function NestLevel3({ children, ...props }) {
   )
 }
 
-function FieldItem(props) {
+function FieldItem({ render, ...props }) {
   logger.log('FieldItem', props)
 
   const handleChange = (onChange) => {
@@ -224,12 +446,13 @@ function FieldItem(props) {
     }
   }
 
-  return props.render({ ...props, onChange: handleChange })
+  return render({ ...props, onChange: handleChange })
 }
 
 function ControlledTextField({ name, ...props }) {
 
   const defaultValue = props.getData(name) ?? ""
+  const label = props.label ?? name
   logger.log('ControlledTextField', name, defaultValue)
 
   return (
@@ -243,7 +466,7 @@ function ControlledTextField({ name, ...props }) {
 
         return (
           <TextField
-            label={`${name} label`}
+            label={label}
             variant="standard"
             size="small"
             sx={{ fontSize: 'small' }}
