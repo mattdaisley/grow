2dd2fe6a11c0f153f29f39071bd5fd7d4e9e6b4e
Matt Daisley <mattdaisley@gmail.com>
2023-02-28 23:25:25 -0700

    Add page without edit controls

---
+++ grow.client/app/testing/[dynamic]/dynamicPageTemplate.js
@@ -0,0 +1,31 @@
+'use client'
+
+import Box from "@mui/material/Box";
+import Grid from '@mui/material/Unstable_Grid2';
+
+import { useItems } from "../nesting/useItems";
+import { ShowItems } from "../nesting/ShowItems";
+import logger from "../../../../grow.api/src/logger";
+
+export default function DynamicEditPageTemplate({ contextKey, filter, itemKeys, data }) {
+
+  const itemsMethods = useItems(itemKeys, data);
+
+  logger.log('TestingNestingPage', itemsMethods.itemKeys)
+
+  if (itemsMethods.itemKeys.length === 0) {
+    return null;
+  }
+
+  return (
+    <Grid xs={12} container sx={{ width: '100%' }}>
+      <Box sx={{ flexGrow: 1, py: 4, px: { xs: 2, md: 4 } }}>
+        <Grid container spacing={4} xs={12} sx={{ width: '100%' }}>
+          {/* <ShowItems contextKey={'preview'} contextValueKeyPrefix={'preview'} itemKey={'pages'} filter={'5ef5fa9b-e8aa-4d00-a500-7bd753d29e34'} itemsMethods={itemsMethods} /> */}
+          {/* <ShowItems contextKey={'preview'} contextValueKeyPrefix={'preview'} itemKey={'pages'} filter={'635fb7cf-ab95-4ce1-a3aa-09bf3a708ac8'} itemsMethods={itemsMethods} /> */}
+          <ShowItems contextKey={contextKey} contextValueKeyPrefix={contextKey} itemKey={'pages'} filter={filter} itemsMethods={itemsMethods} />
+        </Grid>
+      </Box>
+    </Grid>
+  )
+}
---
+++ grow.client/app/testing/[dynamic]/page.js
@@ -0,0 +1,54 @@
+
+import logger from "../../../../grow.api/src/logger";
+import DynamicEditPageTemplate from './dynamicPageTemplate';
+
+export default async function DynamicPage({ params }) {
+
+  const itemKeys = ['pages', 'views', 'fields', 'collections']
+  if (params.dynamic !== undefined) {
+    itemKeys.push(params.dynamic)
+  }
+
+  console.log('DynamicEditPage', 'params:', params, 'itemKeys:', itemKeys)
+
+  const data = await getDynamicData(itemKeys)
+
+  return (
+    <DynamicEditPageTemplate contextKey={params.dynamic} filter={params.filter?.[0]} itemKeys={itemKeys} data={data} />
+  )
+
+}
+
+async function getDynamicData(itemKeys) {
+  const data = {}
+
+
+  const requests = itemKeys.map(itemKey => getItems(itemKey))
+
+  const startTime = new Date();
+
+  const responses = await Promise.all(requests);
+
+  const endTime = new Date();
+  const timeDiff = endTime - startTime; //in ms
+
+  responses.forEach(response => {
+    Object.keys(response).forEach(itemKey => data[itemKey] = response[itemKey])
+  });
+
+
+  logger.log('getDynamicData', `- ${timeDiff}ms - loaded data -`, Object.keys(data).map(itemKey => `${itemKey}: ${data[itemKey].length}`).join(' '))
+
+  return data;
+}
+
+async function getItems(itemKey) {
+  const res = await fetch(`http://192.168.86.249:3001/dynamic/${itemKey}`);
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
