3ecae4e74d37ad91ba89b1c5fa9b56dc50b5c67b
mattdaisley@gmail.com <mattdaisley@gmail.com>
2023-08-21 12:01:53 -0600

    basic login form on home page

--- grow.client/app/(home)/home.js
+++ grow.client/app/(home)/home.js
@@ -1,14 +1,30 @@
 'use client';
 
+import { Button, Paper, TextField, useMediaQuery, useTheme } from '@mui/material';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
 
 export default function HomeComponent() {
 
+  const theme = useTheme();
+  const lessThanSm = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
-    <Container>
-      <Grid xs={12} display="flex" justifyContent="center" alignItems="center">
-        Home Page
+    <Container disableGutters={lessThanSm}>
+      <Grid xs={12} display="flex" flexDirection="column">
+        <Grid sx={{ height: '100vh', maxHeight: '33vh' }}>
+        </Grid>
+        <Grid sx={{ height: '100vh', maxHeight: '34vh' }} container spacing={2} display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+          <Paper sx={{ p: 3, width: { sm: '50%' }}}>
+            <Grid><TextField label="application name" fullWidth /></Grid>
+            <Grid><TextField label="your name" fullWidth /></Grid>
+            <Grid display="flex" justifyContent="center" alignItems="center">
+              <Button variant="outlined" fullWidth >Enter</Button>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid sx={{ height: '100vh', maxHeight: '33vh' }}>
+        </Grid>
       </Grid>
     </Container>
   )
